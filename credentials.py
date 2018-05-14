5e47853bef57969290a132c9e1f1727a3561e760
dragland <dragland@stanford.edu>
2018-05-14 15:53:59 -0700

    Finally got PRAW working.

---
+++ credentials.py
@@ -0,0 +1,8 @@
+#!//usr/bin/python
+#Davy Ragland | dragland@stanford.edu
+#Adrien Truong | aqtruong@stanford.edu 
+#CS231N_REDDIT_NET | 2018
+
+#*********************************** SETUP *************************************
+CLIENT_ID = 'SUPER_SECRET_PASSWORD'
+CLIENT_SECRET = 'SUPER_SECRET_PASSWORD'
--- get_datasets.py
+++ get_datasets.py
@@ -1,10 +1,20 @@
-#!/usr/bin/env python
+#!//usr/bin/python
 #Davy Ragland | dragland@stanford.edu
 #Adrien Truong | aqtruong@stanford.edu 
 #CS231N_REDDIT_NET | 2018
 
 #*********************************** SETUP *************************************
 import praw
+import credentials
 
 #************************************ MAIN *************************************
 print("Executing program:")
+reddit = praw.Reddit(client_id=credentials.CLIENT_ID, client_secret=credentials.CLIENT_SECRET, user_agent='CS231N_REDDIT_NET')
+
+reddits = ["OldSchoolCool", "streetwear", "cosplay", "womensstreetwear", "malefashionadvice", "gaming"]
+
+for subreddit in reddits:
+	print(subreddit + "......................................................")
+	for submission in reddit.subreddit(subreddit).top(limit=10):
+		print(submission.title)
+		print(submission.url)
