ae1c3935a7cb4c1702f69c647148fee6ed49676a
dragland <dragland@stanford.edu>
2018-05-14 14:36:55 -0700

    Started download script.

---
+++ get_datasets.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+#Davy Ragland | dragland@stanford.edu
+#Adrien Truong | aqtruong@stanford.edu 
+#CS231N_REDDIT_NET | 2018
+
+#*********************************** SETUP *************************************
+import praw
+
+#************************************ MAIN *************************************
+print("Executing program:")
