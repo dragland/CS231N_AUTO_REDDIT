e3b769559ff229166b438b4d2436a8ce33f8ec9f
dragland <dragland@stanford.edu>
2018-05-14 17:02:54 -0700

    Got downloading from reddit data working.

--- get_datasets.py
+++ get_datasets.py
@@ -5,16 +5,25 @@
 
 #*********************************** SETUP *************************************
 import praw
+import urllib2
 import credentials
 
 #************************************ MAIN *************************************
 print("Executing program:")
-reddit = praw.Reddit(client_id=credentials.CLIENT_ID, client_secret=credentials.CLIENT_SECRET, user_agent='CS231N_REDDIT_NET')
+reddit = praw.Reddit(client_id=credentials.CLIENT_ID, client_secret=credentials.CLIENT_SECRET, user_agent="CS231N_REDDIT_NET")
 
 reddits = ["OldSchoolCool", "streetwear", "cosplay", "womensstreetwear", "malefashionadvice", "gaming"]
+dataset_path = "datasets/"
 
 for subreddit in reddits:
 	print(subreddit + "......................................................")
-	for submission in reddit.subreddit(subreddit).top(limit=10):
-		print(submission.title)
-		print(submission.url)
+	i = 0
+	for submission in reddit.subreddit(subreddit).top("all", limit=50):
+		filetype = submission.url.split(".")[-1]
+		if filetype == "jpg" or filetype == "png" or filetype == "JPG" or filetype == "PNG":
+			print(str(i) + ": " + submission.url + " [" + str(submission.score) + "]")
+			path = dataset_path + subreddit + str(i) + "." + filetype
+			f = urllib2.urlopen(submission.url)
+			with open(path, "wb") as output:
+			  output.write(f.read())
+			i += 1
