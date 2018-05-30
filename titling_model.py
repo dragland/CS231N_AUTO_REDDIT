62e6440d037eb0f0d1de25d3905f2d5844e6e296
Adrien Truong <adrien.truong@me.com>
2018-05-30 04:01:11 -0700

    Simplify inference models

--- titling_model.py
+++ titling_model.py
@@ -24,7 +24,9 @@ class ImageTitlingModel(object):
         self.words_by_id = words_by_id
         self.id_by_words = id_by_words
 
-        self.create_models(embedding_matrix, num_subreddits, max_len)
+        self.lstm_size = 256
+
+        self.create_models(self.lstm_size, embedding_matrix, num_subreddits, max_len)
 
     def load_checkpoint(self, save_file):
         self.train_model = load_model(save_file)
@@ -39,10 +41,15 @@ class ImageTitlingModel(object):
         subreddit_one_hot[subreddit] = 1
         encoder_output = self.inference_encoder_model.predict([np.array([img]), np.array([subreddit_one_hot])])
 
-        stop_condition = False
-        prev_word = self.id_by_words[START_TOKEN]
-        prev_h = encoder_output
-        prev_c = np.zeros(prev_h.shape)
+        zero_h = np.zeros((encoder_output.shape[0], self.lstm_size))
+        zero_c = np.zeros((encoder_output.shape[0], self.lstm_size))
+        _, initial_h, initial_c = self.inference_decoder_model.predict([encoder_output, zero_h, zero_c])
+
+        start_id = self.id_by_words[START_TOKEN]
+        start_embedding = self.embedding_matrix[start_id]
+        prev_h = initial_h
+        prev_c = initial_c
+        prev_word = start_embedding
         title = []
         end_id = self.id_by_words[END_TOKEN]
         while len(title) < self.max_len:
@@ -55,40 +62,40 @@ class ImageTitlingModel(object):
             predicted_word = self.words_by_id[predicted_word_id]
             title.append(predicted_word)
 
-            prev_word = predicted_word_id
+            prev_word = self.embedding_matrix[predicted_word_id]
             prev_h = h
             prev_c = c
 
         return ' '.join(title)
 
     def set_inference_weights_from_train(self):
-        inference_layers = self.inference_encoder_model.layers + self.inference_decoder_model.layers
+        inference_models = [self.inference_encoder_model, self.inference_decoder_model]
+        inference_layers = []
+        for model in inference_models:
+            inference_layers += model.layers
         train_layers_by_name = {layer.name: layer for layer in self.train_model.layers}
         for inference_layer in inference_layers:
             train_layer = train_layers_by_name[layer.name]
             inference_layer.weights = train_layer.weights
 
-    def create_models(self, embedding_matrix, num_subreddits, max_len):
+    def create_models(self, lstm_size, embedding_matrix, num_subreddits, max_len):
         cnn_encoder = VGG16(weights='imagenet', include_top=False)
         for layer in cnn_encoder.layers:
             layer.trainable = False
 
-        lstm_hidden_size = 256
+        vocab_size = embedding_matrix.shape[0]
+        word_embeddings_size = embedding_matrix.shape[1]
 
         one_hot_subreddit = Input(shape=(num_subreddits,), dtype='float32', name='subreddit_input')
         features = cnn_encoder.output
         features = GlobalAveragePooling2D()(features)
         features_concat = Concatenate()([features, one_hot_subreddit])
 
-        encoder_output = Dense(lstm_hidden_size, name=PROJECTION_LAYER)(features_concat)
-        encoder_inputs = [cnn_encoder.inputs[0], one_hot_subreddit]
+        encoder_output = Dense(word_embeddings_size, name=PROJECTION_LAYER)(features_concat)
 
-        # decoder changes between training and testing
+        # decoding changes between training and testing
         # during training, we feed the ground truth prev word into the LSTM
         # during inference, we feed the output of the LSTM back into the LSTM
-
-        vocab_size = embedding_matrix.shape[0]
-        word_embeddings_size = embedding_matrix.shape[1]
 
         # training model
         train_titles = Input(shape=(max_len,), dtype='int32', name='train_titles_input')
@@ -101,30 +108,27 @@ class ImageTitlingModel(object):
             name=EMBEDDING_LAYER)
         train_embeddings = train_embedding_layer(train_titles)
 
-        train_decoder = LSTM(lstm_hidden_size, return_sequences=True, name=LSTM_LAYER)
-        train_hidden_states = train_decoder(train_embeddings, initial_state=[encoder_output, encoder_output])
+        encoder_output_reshaped = Reshape((1, -1))(encoder_output)
+        train_decoder = LSTM(lstm_size, return_sequences=True, return_state=True, name=LSTM_LAYER)
+        _, train_initial_h, train_initial_c = train_decoder(encoder_output_reshaped)
+        train_hidden_states, _, _ = train_decoder(train_embeddings, [train_initial_h, train_initial_c])
         train_scores = TimeDistributed(Dense(vocab_size, name=SOFTMAX_LAYER))(train_hidden_states)
         train_probs = Activation('softmax')(train_scores)
 
         self.train_model = Model(inputs=[cnn_encoder.inputs[0], one_hot_subreddit, train_titles], outputs=[train_probs])
 
-        # inference decoder
-        prev_word = Input(shape=(1,), dtype='int32', name='prev_word')
-        prev_h = Input(shape=(lstm_hidden_size,), dtype='float32', name='prev_h')
-        prev_c = Input(shape=(lstm_hidden_size,), dtype='float32', name='prev_c')
-       
-        inference_embedding_layer = Embedding(vocab_size,
-            word_embeddings_size,
-            weights=[embedding_matrix],
-            input_length=1,
-            trainable=False,
-            name=EMBEDDING_LAYER)
-        inference_embeddings = inference_embedding_layer(prev_word)
+        # inference encoder
+        self.inference_encoder_model = Model(inputs=[cnn_encoder.inputs[0], one_hot_subreddit], outputs=encoder_output)
 
-        inference_decoder = LSTM(lstm_hidden_size, return_sequences=True, return_state=True, name=LSTM_LAYER)
-        inference_h, state_h, state_c = inference_decoder(inference_embeddings, initial_state=[prev_h, prev_c])
+        # inference decoder for words
+        prev_word = Input(shape=(word_embeddings_size,), dtype='float32', name='prev_word')
+        prev_h = Input(shape=(lstm_size,), dtype='float32', name='prev_h')
+        prev_c = Input(shape=(lstm_size,), dtype='float32', name='prev_c')
+
+        inference_decoder = LSTM(lstm_size, return_sequences=True, return_state=True, name=LSTM_LAYER)
+        prev_word_reshaped = Reshape((1, -1))(prev_word)
+        inference_h, state_h, state_c = inference_decoder(prev_word_reshaped, initial_state=[prev_h, prev_c])
         inference_scores = Dense(vocab_size, name=SOFTMAX_LAYER)(inference_h)
         inference_probs = Activation('softmax')(inference_scores)
 
-        self.inference_encoder_model = Model(inputs=[cnn_encoder.inputs[0], one_hot_subreddit], outputs=encoder_output)
         self.inference_decoder_model = Model(inputs=[prev_word, prev_h, prev_c], outputs=[inference_probs, state_h, state_c])
